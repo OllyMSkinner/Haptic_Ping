bc829ec407876fbe2df7276ce64cdd8c910b7782
wikismo <wiktoriasmolarek@icloud.com>
2026-03-27 12:12:17 +0000

    Fix I2C error message and update string formatting

--- Piezo /ads1115rpi.cpp
+++ Piezo /ads1115rpi.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <format>
+#include <string>
 
 void ADS1115rpi::start(ADS1115settings settings)
 {
@@ -17,7 +18,7 @@ void ADS1115rpi::start(ADS1115settings settings)
 	{
 		char i2copen[] = "Could not open I2C.\n";
 #ifdef DEBUG
-		fprintf(stderr, i2open);
+		fprintf(stderr, i2copen);
 #endif
 		throw std::invalid_argument(i2copen);
 	}
@@ -49,8 +50,8 @@ void ADS1115rpi::start(ADS1115settings settings)
 	fprintf(stderr, "Receiving data.\n");
 #endif
 
-	const std::string chipPath = std::format("/dev/gpiochip{}", settings.drdy_chip);
-	const std::string consumername = std::format("gpioconsumer_{}_{}", settings.drdy_chip, settings.drdy_gpio);
+	const std::string chipPath = "/dev/gpiochip" + std::to_string(settings.drdy_chip);
+	const std::string consumername = "gpioconsumer_" + std::to_string(settings.drdy_chip) + "_" + std::to_string(settings.drdy_gpio);
 
 	// Config the pin as input and detecting falling and rising edegs
 	gpiod::line_config line_cfg;
